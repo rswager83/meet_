7178ae3e57ccc99f7a328473f169ebccb982b603
rswager83 <101044506+rswager83@users.noreply.github.com>
2022-07-22 09:26:28 +0200

    Update Event.js
    Updating this.props in order to show the props in the event component

--- src/Event.js
+++ src/Event.js
@@ -10,15 +10,16 @@ class Event extends Component {
   }
 
   render() {
+    console.log(this.props.event);
     return (
       // 1-7
       <div className="event">
-        <div className="summary">{this.props.summary}</div>
-        <div className="description">{this.props.description}</div>
-        <div className="creator">{this.props.creator}</div>
-        <div className="location">{this.props.location}</div>
-        <div className="start">{this.props.start}</div>
-        <div className="end">{this.props.end}</div>
+        <div className="summary">{this.props.event.summary}</div>
+        <div className="description">{this.props.event.description}</div>
+        <div className="creator">{this.props.event.creator.email}</div>
+        <div className="location">{this.props.event.location}</div>
+        <div className="start">{this.props.event.start.dateTime}</div>
+        <div className="end">{this.props.event.end.dateTime}</div>
         <button
           onClick={() => this.handleShowDetails()} // 10b & 11b
           className="btn-details" // 8
